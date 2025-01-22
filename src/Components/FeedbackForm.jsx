b2a051e64b54c1634137f3ae80c2c508215cb0a1
Roman Aliaskarov <r.aliaskarov@gmail.com>
2025-01-22 08:17:14 +0100

    added rating radio

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -8,6 +8,7 @@ const FeedbackForm = () => {
     name: '',
     email: '',
     feedback: '',
+    rating: '',
   });
 
   const handleChange = (event) => {
@@ -24,6 +25,7 @@ const FeedbackForm = () => {
       Name: ${formData.name}
       Email: ${formData.email}
       Feedback: ${formData.feedback}
+      Rating: ${formData.rating}
       `;
     const isConfirmed = window.confirm(`Please confirm your details:\n\n${confirmationMessage}`);
     if (isConfirmed) {
@@ -32,6 +34,7 @@ const FeedbackForm = () => {
         name: '',
         email: '',
         feedback: '',
+        rating: '',
       });
       alert('Than you for your feedback!');
   }
@@ -64,6 +67,39 @@ const FeedbackForm = () => {
           value={formData.feedback}
           onChange={handleChange}
           ></textarea>
+          <div style={{display:'flex',gap:'10px',flexDirection:'column'}}>
+            <span>Rate Us:</span>
+            <p><input
+              type="radio"
+              name="rating"
+              value="1"
+              onChange={handleChange}
+            /> 1</p>
+            <p><input
+              type="radio"
+              name="rating"
+              value="2"
+              onChange={handleChange}
+            /> 2</p>
+            <p><input
+              type="radio"
+              name="rating"
+              value="3"
+              onChange={handleChange}
+            /> 3</p>
+            <p><input
+              type="radio"
+              name="rating"
+              value="4"
+              onChange={handleChange}
+            /> 4</p>
+            <p><input
+              type="radio"
+              name="rating"
+              value="5"
+              onChange={handleChange}
+            /> 5</p>
+          </div>
           <button type="submit">Submit Feedback</button>     
       </form>
     </>
